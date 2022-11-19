ae36487cbff759856dee8e18d6020d77f3cae1fc
Viktoras760 <35394736+Viktoras760@users.noreply.github.com>
2022-11-19 10:21:52 +0200

    startup.sh changes discarded

--- startup.sh
+++ startup.sh
@@ -2,10 +2,10 @@
 
 echo "Copying custom default over to /etc/nginx/sites-available/default"
 
-NGINX_CONF=/home/site/default
+NGINX_CONF=/home/default
 
 if [ -f "$NGINX_CONF" ]; then
-    cp /home/site/default /etc/nginx/sites-available/default
+    cp /home/default /etc/nginx/sites-available/default
     service nginx reload
 else
     echo "File does not exist, skipping cp."
